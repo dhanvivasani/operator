b55558ea509009bd23c82d195f338222a5005365
dhanvivasani <132878983+dhanvivasani@users.noreply.github.com>
2023-05-09 22:25:56 +0530

    Add files via upload

---
+++ 4083-03.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+
+{
+
+   int x=2,y=6,xy;
+
+   xy=(x*x*x)+((3*x*y)*(x+y))+(y*y*y);
+   printf("%d",xy);
+
+   getch();
+
+   }
---
+++ 4083-04.C
@@ -0,0 +1,17 @@
+ #include<stdio.h>
+ #include<conio.h>
+
+
+ main()
+
+ {
+
+  int x=9,y=3,xy;
+   clrscr();
+  xy=(x*x*x)-(y*y*y)-((3*x*y)*(x+y));
+
+  printf("%i",xy);
+
+  getch();
+
+  }
