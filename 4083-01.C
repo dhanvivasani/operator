d0444686aa13b28da0ee6e6aecfdf2eaa2b721a0
dhanvivasani <132878983+dhanvivasani@users.noreply.github.com>
2023-05-09 12:55:00 +0530

    Add files via upload

---
+++ 4083-01.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+
+{
+  int x=5,y=3,xy;
+
+  xy=(x*x)+(2*x*y)+(y*y);
+  printf("%d",xy);
+
+  getch();
+
+  }
---
+++ 4083-02.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+
+{
+    int x=6,y=8,xy;
+    xy=(x*x)-(2*x*y)+(y*y);
+
+    printf("%d",xy);
+
+    getch();
+    }
